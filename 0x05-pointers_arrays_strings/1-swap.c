2150cd4527018374c8bf66c321b9ea53d10c5c00
mwangiNjogu <njogueustace2@gmail.com>
2022-09-19 10:49:41 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int -  swaps two interger' values
+ * @a: first interger
+ * @b: second interger
+ * Return: returns nothing
+ */
+
+void swap_int(int *a, int *b);
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b - tmp;
+}
+
